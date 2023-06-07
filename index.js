39cd25c21f84c4a20aad32664f4e9e88bab91722
Vu Duy Tu <tu.vuduy@vti.com.vn>
2023-06-08 00:37:00 +0700

    v4

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get('/', (req, res) => {
 	console.log('Hello root!');
-	res.json('Hello Vu Duy Tu v3!');
+	res.json('Hello Vu Duy Tu v4!');
 });
 
 app.listen(8080, () => {
-	console.log('App started successfully v3 !');
+	console.log('App started successfully v4!');
 })
